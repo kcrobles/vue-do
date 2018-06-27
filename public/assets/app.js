99f073e6be0d9141c1e8c6c775907bf0a1855e43
Kenny Chancafe <kendu.script@gmail.com>
2018-06-27 17:00:20 -0300

    app

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,33 +1,41 @@
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 };
+const COMPLETED = "completed";
+const PENDING = "pending";
 
 Vue.component('todo-list', {
-  props: ['todos', 'deleter'],
+  props: ['todos', 'deleter', 'statuschange', 'filters'],
   template: `
-    <ul class="todo-list">
+    <ul class="todo-list" v-if="todos.length > 0">
       <todo-item 
         v-for="(todo, index) in todos" 
         :key="index"
         v-bind:description="todo.description"
         v-bind:status="todo.status" 
         v-bind:deleter="deleter"
+        v-bind:onstatuschange="statuschange"
       />
     </ul>
   `
 });
 
 Vue.component('todo-item', {
-  props: ['description', 'status', 'deleter'],
+  props: ['description', 'status', 'deleter', 'onstatuschange'],
   methods: {
     handleDelete: function(event) {
       this.deleter(this.$vnode.key);
+    },
+    handleStatus: function(event) {
+      this.onstatuschange(this.$vnode.key);
     }
   },
   template: `
     <li v-if="status" :class="['todo-list-item', status]">
-      <div>{{ description }}</div>
-      <i @click="handleDelete" class="fab fa-algolia fa-2x"></i>
+      <div>
+        <i @click="handleStatus" :class="['far', status === 'pending' ? 'fa-circle' : 'fa-check-circle']">
+        </i><span>{{ description }}</span></div>
+      <i @click="handleDelete" class="fas fa-times hide-hover"></i>
     </li>
   `
 });
@@ -67,12 +75,36 @@ Vue.component('add-todo', {
   `
 });
 
+Vue.component('filter-selector', {
+  props: ['filters', 'filterchange'],
+  methods: {
+    handleClick: function(index) {
+      this.filterchange(index);
+    }
+  },
+  template: `
+    <div class="filter-selector-box">
+      <div 
+        v-for="(filter, index) in filters" 
+        :class="['filter-item', filter.selected ? 'active' : '']" 
+        :key="index" 
+        @click="handleClick(index)"
+      >{{ filter.name }}</div>
+    </div>
+  `
+});
+
 const app = new Vue({
   el: '#app',
   data: {
     title: 'Vuetodo App',
     another: '',
-    todos: []
+    todos: [],
+    filters: [
+      { name: 'all', selected: true },
+      { name: 'pending', selected: false },
+      { name: 'completed', selected: false }
+    ]
   },
   mounted() {
     const data = localStorage.getItem('vuetodo');
@@ -86,7 +118,7 @@ const app = new Vue({
       if (text !== undefined && text.length > 0) {
         this.todos.push({
           description: text.capitalize(),
-          status: 'pending'
+          status: PENDING
         });
         this.save();
       }
@@ -100,6 +132,33 @@ const app = new Vue({
     deleter: function(index) {
       this.todos.splice(index, 1);
       this.save();
+    },
+    statuschange: function(index) {
+      this.todos = this.todos.map((todo, idx) => {
+        if(idx === index) {
+          let { status } = todo;
+          status = status === PENDING ? COMPLETED : PENDING;
+          return {...todo, status: status};
+        }
+        return todo;
+      });
+    },
+    filterchange: function(index) {
+      this.filters = this.filters.map((filter, idx) => {
+        if(idx === index) {
+          return {...filter, selected: true };
+        }
+        return {...filter, selected: false };
+      });
+    },
+    filtered: function() {
+      const { name } = this.filters.filter((f) => f.selected === true)[0];
+      return this.todos.filter(function(todo) {
+        if (name !== 'all') {
+          return todo.status === name;
+        } 
+        return todo;                
+      });
     }
   }
 });
